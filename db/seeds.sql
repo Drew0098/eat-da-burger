a56e64834d00b34c4eb0a7218f9931c27554ca80
Drew0098 <a.eaton0098@gmail.com>
2018-04-21 03:12:56 -0400

    Done

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Truffle Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', FALSE);
